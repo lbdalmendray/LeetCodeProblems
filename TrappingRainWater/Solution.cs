dc38a08a653c726046e185d8e1de2667110acaeb
Luis <lbdalmendray@gmail.com>
2019-02-24 23:10:25 -0500

    TrappingRainWater Problem . Solution o(n^2 )

---
+++ TrappingRainWater/Solution.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrappingRainWater
+{
+    public class Solution
+    {
+        public int Trap(int[] height)
+        {
+            if (height.Length == 0)
+                return 0;
+
+            return Trap(height, 0);
+        }
+
+        public int Trap(int[] height, int i)
+        {
+            if (i == height.Length - 1)
+            {
+                return 0;
+            }
+
+            if (height[i] <= height[i + 1])
+            {
+                return Trap(height, i + 1);
+            }
+            else
+            {
+                int lastWaterIndexSide = -1;
+                if (ExistWaterZone(height, i, out lastWaterIndexSide))
+                {
+                    return CalculateWater(height, i, lastWaterIndexSide) + Trap(height, lastWaterIndexSide);
+                }
+                else
+                {
+                    return Trap(height, i + 1);
+                }
+            }
+        }
+
+        public int CalculateWater(int[] height, int i, int lastWaterIndexSide)
+        {
+            int sum = 0;
+            int maxHeigth = Math.Min(height[i], height[lastWaterIndexSide]);
+
+            for (int j= i+1 ;  j<= lastWaterIndexSide-1; j++)
+            {
+                sum += height[j];
+            }
+            sum += 2*maxHeigth;
+
+            int result = maxHeigth * (lastWaterIndexSide - i + 1);
+            result -= sum;
+
+            return result;
+        }
+
+        private bool ExistWaterZone(int[] height, int i, out int lastIndexSide)
+        {
+            lastIndexSide = i+1;
+            if (i == height.Length - 2) // Three indexes at least 
+                return false;
+
+            for (int k = i+2; k < height.Length; k++)
+            {
+                if ( height[k]>= height[i])
+                {
+                    lastIndexSide = k;
+                    return true;
+                }
+                else if ( height[k] > height[lastIndexSide] )
+                {
+                    lastIndexSide = k;
+                }
+            }
+
+            if (height[lastIndexSide] > height[i + 1])
+                return true;
+            
+            return false;
+        }
+    }
+}
---
+++ TrappingRainWaterTest/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrappingRainWater;
+
+namespace TrappingRainWaterTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int result = s.Trap(new int[] { 0,1,0,1,2});
+            Assert.AreEqual(result, 1); //[4,2,3]
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int result = s.Trap(new int[] { 0, 1, 0, 2});
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int result = s.Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3 });
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int result = s.Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3 });
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int result = s.Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 });
+            Assert.AreEqual(result, 6);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution s = new Solution();
+            int result = s.Trap(new int[] { 4, 2, 3 });
+            Assert.AreEqual(result, 1); 
+        }
+
+        [TestMethod]
+        public void CalculateWaterZone1()
+        {
+            Solution s = new Solution();
+            int result = s.CalculateWater(new int[] { 1, 0, 1 }, 0, 2);
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void CalculateWaterZone2()
+        {
+            Solution s = new Solution();
+            int result = s.CalculateWater(new int[] { 5,1, 2, 8 }, 0, 3);
+            Assert.AreEqual(result, 5*4 - (5+1+2+5));
+        }
+    }
+}
