d24c889f281a2c284a2ac7d26c297013dd35a39c
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2019-12-16 00:43:46 -0500

    ShortestPathinaGridwithObstaclesElimination problem

---
+++ ShortestPathinaGridwithObstaclesElimination/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPathinaGridwithObstaclesElimination
+{
+    public class Solution
+    {
+        public int ShortestPath(int[][] grid, int k)
+        {
+            if ( k >= grid.Length + grid[0].Length - 3)
+            { 
+                return grid.Length + grid[0].Length - 2;
+            }
+
+            BFSInfo[,] infos = new BFSInfo[grid.Length, grid[0].Length];
+            int?[,,]  solutions = new int?[grid.Length, grid[0].Length, k + 1];
+            for (int i = 0; i <=k ; i++)
+            {
+                solutions[grid.Length-1, grid[0].Length-1, i] = 0; 
+            }
+
+            return ShortestPath(0, 0, k, grid, infos, solutions);
+        }
+
+        private int ShortestPath(int x, int y, int k, int[][] grid, BFSInfo[,] infos, int?[,,] solutions)
+        {
+            if (solutions[x, y, k].HasValue)
+                return solutions[x, y, k].Value;
+
+            if(infos[x,y] == null)
+            {
+                infos[x, y] = CalculateBFSInfo(x, y, grid);
+            }
+            int? result = null;
+            if (infos[x, y].Result.HasValue)
+                result = infos[x, y].Result.Value;
+
+            if (k > 0)
+            {
+                foreach (var obstacle in infos[x, y].Obstacles)
+                {
+                    var currentResult = ShortestPath(obstacle[0], obstacle[1], k - 1, grid, infos, solutions);
+                    if (currentResult != -1)
+                    {
+                        currentResult += obstacle[2];
+                        if (!result.HasValue || result > currentResult)
+                            result = currentResult;
+                    }
+                }
+            }
+
+            solutions[x, y, k] = result.HasValue ? result.Value : -1;
+            return solutions[x, y, k].Value;
+        }
+
+        private BFSInfo CalculateBFSInfo(int x, int y, int[][] grid)
+        {
+            BFSInfo result = new BFSInfo();
+            bool[,] selected = new bool[grid.Length, grid[0].Length];
+
+            LinkedList<int[]> queue = new LinkedList<int[]>();
+            queue.AddLast(new int[] { x, y, 0 });
+           
+            while(queue.Count != 0)
+            {
+                var value = queue.First.Value;
+                queue.RemoveFirst();
+
+                if (value[0] == grid.Length - 1 && value[1] == grid[0].Length-1)
+                {
+                    result.Result = value[2];
+                    var lessValue = value[2] - 1;
+                    while(result.Obstacles.Count != 0)
+                    {
+                        if (result.Obstacles.Last.Value[2] >= lessValue)
+                        {
+                            result.Obstacles.RemoveLast();
+                        }
+                        else
+                            break;
+                    }
+                    break;
+                }
+                IEnumerable<int[]> nextPositions = GetNextPositions(value,grid);
+                var nextValue = value[2] + 1;
+                foreach (var item in nextPositions.Where(e => !selected[e[0], e[1]]))
+                {
+                    if( grid[item[0]][item[1]] == 1)
+                    {
+                        result.Obstacles.AddLast(new int[] { item[0], item[1], nextValue });
+                    }
+                    else
+                    {
+                        queue.AddLast(new int[] { item[0], item[1], nextValue });
+                    }
+                    selected[item[0], item[1]] = true;
+                }                
+            }
+
+            return result;
+        }
+
+        private IEnumerable<int[]> GetNextPositions(int[] value, int[][] grid)
+        {
+            LinkedList<int[]> result = new LinkedList<int[]>();
+            if (value[0] > 0)
+                result.AddLast(new int[] { value[0] - 1, value[1] });
+            if (value[0] < grid.Length - 1)
+                result.AddLast(new int[] { value[0] + 1, value[1] });
+            if (value[1] > 0)
+                result.AddLast(new int[] { value[0] , value[1] - 1 });
+            if (value[1] < grid[0].Length - 1)
+                result.AddLast(new int[] { value[0] , value[1] + 1 });
+            return result;
+        }
+    }
+
+    internal class BFSInfo
+    {
+        public int? Result { get; internal set; }
+        public LinkedList<int[]> Obstacles { get; internal set; } = new LinkedList<int[]>();
+    }
+}
---
+++ ShortestPathinaGridwithObstaclesEliminationTest/UnitTest1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShortestPathinaGridwithObstaclesElimination;
+using Utils;
+
+namespace ShortestPathinaGridwithObstaclesEliminationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = Utils.Utils.ParseArrayArrayInteger("[[0,0,0],[1, 1, 0],[0, 0, 0], [0, 1, 1], [0, 0, 0]]");
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 1);
+            Assert.AreEqual(result, 6);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var input = Utils.Utils.ParseArrayArrayInteger("[[0,1,1],[1, 1, 1],[1, 0, 0]]");
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 1);
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var input = Utils.Utils.ParseArrayArrayInteger("[[0,1,1],[1, 1, 1],[0, 0, 0]]");
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 1);
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var origInput = Utils.Utils.ParseArrayArrayInteger("[[0,1,1],[1, 1, 1],[0, 0, 0]]");
+            var input = ConvertToLength(origInput,40);
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 1);
+            Assert.AreEqual(result, 4 + 40- origInput.Length + 40- origInput[0].Length);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var origInput = Utils.Utils.ParseArrayArrayInteger("[[0,1,1],[1, 1, 1],[1, 0, 0]]");
+            var input = ConvertToLength(origInput, 40);
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 1);
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var origInput = Utils.Utils.ParseArrayArrayInteger("[[0,0,0],[1, 1, 0],[0, 0, 0], [0, 1, 1], [0, 0, 0]]");
+            var input = ConvertToLength(origInput, 40);
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 1);
+            Assert.AreEqual(result, 6 + 40 - origInput.Length + 40 - origInput[0].Length);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            var input = Utils.Utils.ParseArrayArrayInteger("[[0]]");
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 0);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            var input = Utils.Utils.ParseArrayArrayInteger("[[0,0]]");
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 0);
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            var input = Utils.Utils.ParseArrayArrayInteger("[[0,1,0]]");
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 0);
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            var input = Utils.Utils.ParseArrayArrayInteger("[[0,1,1],[1, 1, 1],[1, 1, 1], [1, 1, 1], [1, 1, 0]]");
+            Solution s = new Solution();
+            var result = s.ShortestPath(input, 5);
+            Assert.AreEqual(result, 6);
+        }
+
+        int [][] ConvertToLength(int[][] input , int length)
+        {
+            int[][] result = new int[length][];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = new int[length];
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[0].Length; j++)
+                {
+                    result[i][j] = input[i][j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
