22ca51b4c4d344b6ffa7bb94d17f780bef1f008d
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-10-13 23:06:04 -0400

    Updated

---
+++ SpiralMatrix/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralMatrix
+{
+    public class Solution
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            if ( matrix == null || matrix.Length == 0 || matrix[0].Length == 0  )
+                return new List<int>();
+            
+            int n = matrix.Length ;            
+            int m = matrix[0].Length ;            
+
+            LinkedList<int> result = new LinkedList<int>();
+        
+            int x = 0;
+            int y = 0;
+            
+            while( n > 1 && m > 1)
+            {
+                for( int i = 0 + y ; i < y + m ; i ++)              
+                {
+                    result.AddLast(matrix[x][i]);                    
+                }
+                y += m - 1 ;
+                for( int i = 1 + x ; i < x + n-1 ; i ++)              
+                {
+                    result.AddLast(matrix[i][y]);                    
+                }
+                x+= n-1;
+                for( int i = y - 0 ; i > y - m ; i--)
+                {
+                     result.AddLast(matrix[x][i]);     
+                }
+                y -= m-1;
+                for ( int i = x - 1 ; i > x - ( n - 1 ) ; i--)        
+                {
+                    result.AddLast(matrix[i][y]);
+                }
+                x -= (n - 2);
+                y++;
+                n -= 2;
+                m -= 2;                              
+            }     
+
+            if( n == 1)
+            {
+                for( int i = 0 + y ; i < y + m ; i ++)              
+                {
+                    result.AddLast(matrix[x][i]);                    
+                }                
+            }
+            else if ( m == 1)
+            {
+                for( int i = 0 + x ; i < x + n ; i ++)              
+                {
+                    result.AddLast(matrix[i][y]);                    
+                }
+            }       
+            
+            return result.ToList();
+        }
+    }
+}
---
+++ SpiralMatrixTest/UnitTest1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpiralMatrix;
+
+namespace SpiralMatrixTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[][] input = new int[][]
+            {
+                new int[]{1, 2, 3 },
+                new int[]{4, 5, 6},
+                new int[]{7, 8, 9}
+            };
+
+            Solution s = new Solution();
+            var result =  s.SpiralOrder(input);
+            int[] expected = new int[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+            CollectionAssert.AreEquivalent(result.ToList(), expected.ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[][] input = new int[][]
+            {
+                new int[]{1, 2, 3, 4 },
+                new int[]{5, 6, 7, 8},
+                new int[]{ 9, 10, 11, 12 }
+            };
+
+            Solution s = new Solution();
+            var result = s.SpiralOrder(input);
+            int[] expected = new int[] { 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7 };
+            CollectionAssert.AreEquivalent(result.ToList(), expected.ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[][] input = new int[][]
+            {
+                new int[]{8}
+            };
+
+            Solution s = new Solution();
+            var result = s.SpiralOrder(input);
+            int[] expected = new int[] { 8 };
+            CollectionAssert.AreEquivalent(result.ToList(), expected.ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[][] input = new int[][]
+            {
+                new int[]{}
+            };
+
+            Solution s = new Solution();
+            var result = s.SpiralOrder(input);
+            int[] expected = new int[] { };
+            CollectionAssert.AreEquivalent(result.ToList(), expected.ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int[][] input = new int[][]
+            {
+                new int[]{ 1, 2 , 3 ,4 ,5 , 6 }
+            };
+
+            Solution s = new Solution();
+            var result = s.SpiralOrder(input);
+            int[] expected = new int[] { 1, 2, 3, 4, 5, 6  };
+            CollectionAssert.AreEquivalent(result.ToList(), expected.ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int[][] input = new int[][]
+            {
+                new int[]{1} ,
+                new int[]{2 },
+                new int[]{3 } ,
+                new int[]{4 } ,
+                new int[]{5 } ,
+                new int[]{6 }
+            };
+
+            Solution s = new Solution();
+            var result = s.SpiralOrder(input);
+            int[] expected = new int[] { 1, 2, 3, 4, 5, 6 };
+            CollectionAssert.AreEquivalent(result.ToList(), expected.ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int[][] input = new int[][]
+            {
+                new int[]{1,    2,  3   ,   4   ,   5   ,   6   ,   7 },
+                new int[]{24,   25, 26  ,   27  ,   28  ,   29  ,   8 },
+                new int[]{23,   40, 41  ,   42  ,   43  ,   30  ,   9 },
+                new int[]{22,   39, 48  ,   49  ,   44  ,   31  ,   10 },
+                new int[]{21,   38, 47  ,   46  ,   45  ,   32  ,   11 },
+                new int[]{20,   37, 36  ,   35  ,   34  ,   33  ,   12 },
+                new int[]{19,   18, 17  ,   16  ,   15  ,   14  ,   13 },
+            };
+
+            Solution s = new Solution();
+            var result = s.SpiralOrder(input);
+            int[] expected = Enumerable.Range(1,49).ToArray();
+            CollectionAssert.AreEquivalent(result.ToList(), expected.ToList());
+        }
+    }
+}
