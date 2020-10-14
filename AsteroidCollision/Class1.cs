3d0a4f2fc4bcda212aabde3cb380b55356c2a8fe
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-10-13 21:07:26 -0400

    Updated

---
+++ AsteroidCollision/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AsteroidCollision
+{
+    public class Solution
+    {
+        /*
+         Problem(i,N-1) = 
+        case1 : if(asteroids[i] < 0) -> Result = asteroids[i] + Problem(i+1,N-1)
+        case2 : if(asteroids[i] == 0) -> Result = Problem(i+1,N-1)[0] < 0 -> Result = Problem(i+1,N-1)
+                                      -> Result = Problem(i+1,N-1)[0] >=0 -> Result = asteroids[i] + Problem(i+1,N-1)
+        case3 : if(asteroids[i] > 0 ) -> Result = Problem(i+1,N-1)[0] < 0 && Abs(asteroids[i]) == Abs(Problem(i+1,N-1)[0]) -> Result = Problem(i+1,N-1).EliminateFirstElement()
+                                      -> Result = Problem(i+1,N-1)[0] < 0 && Abs(asteroids[i]) < Abs(Problem(i+1,N-1)[j]) j = 0,...,k-1  -> Result = Problem(i+1,N-1).EliminateFirstKElements() OR just asteroids[i]
+                                      -> Result = Problem(i+1,N-1)[0] > 0 -> Result = asteroids[i] + Problem(i+1,N-1)
+                                      -> Result = Problem(i+1,N-1)[0] == 0 -> Result = asteroids[i] + Problem(i+1,N-1).EliminateFirstElement()
+         */
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.AddFirst(asteroids[asteroids.Length - 1]);
+
+            for (int i = asteroids.Length - 2 ; i > -1 ; i--)
+            {
+                if (asteroids[i] == 0)
+                {
+                    if (list.Count != 0)
+                    {
+                        if (list.First.Value >= 0)
+                        {
+                            list.AddFirst(asteroids[i]);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        list.AddFirst(asteroids[i]);
+                    }    
+                }
+                else if ( asteroids[i] < 0)
+                {
+                    list.AddFirst(asteroids[i]);
+                }
+                else // asteroids[i] > 0 
+                {
+                    while(list.Count != 0 && list.First.Value <= 0 && Math.Abs(asteroids[i]) > Math.Abs( list.First.Value )  )
+                    {
+                        list.RemoveFirst();
+                    }
+
+                    if( list.Count != 0 )
+                    {
+                        if (list.First.Value <= 0)
+                        {
+                            if (Math.Abs(asteroids[i]) == Math.Abs(list.First.Value))
+                            {
+                                list.RemoveFirst();
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            list.AddFirst(asteroids[i]);
+                        }
+                    }
+                    else
+                    {
+                        list.AddFirst(asteroids[i]);
+                    }
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+
+    
+}
---
+++ AsteroidCollisionTest/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AsteroidCollision;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsteroidCollisionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            var result = s.AsteroidCollision(new[] { 5, 10, -5 });
+            CollectionAssert.AreEquivalent(result, new[] { 5, 10 });
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            var result = s.AsteroidCollision(new[] { 8, -8 });
+            CollectionAssert.AreEquivalent(result, new int[] { });
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            var result = s.AsteroidCollision(new[] { 10, 2, -5 });
+            CollectionAssert.AreEquivalent(result, new int[] { 10 });
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            var result = s.AsteroidCollision(new[] { -2, -1, 1, 2 });
+            CollectionAssert.AreEquivalent(result, new int[] { -2, -1, 1, 2 });
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            var result = s.AsteroidCollision(new[] { 0,0,0 });
+            CollectionAssert.AreEquivalent(result, new int[] { 0,0,0 });
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution s = new Solution();
+            var result = s.AsteroidCollision(new[] { 0, 0, 0 , -2 , 3 , -5});
+            CollectionAssert.AreEquivalent(result, new int[] { -2, -5 });
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Solution s = new Solution();
+            var result = s.AsteroidCollision(new[] { -2, 0, 10, 0, 0, -2, 3, -5 });
+            CollectionAssert.AreEquivalent(result, new int[] { -2, 0, 10 });
+        }
+    }
+
+}
