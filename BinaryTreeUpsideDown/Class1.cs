05adc7c6eeea3aa3c97ad2c647c766c126251dbe
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-10-14 23:55:43 -0400

    Updated

---
+++ BinaryTreeUpsideDown/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeUpsideDown
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+    public class Solution
+    {
+        public TreeNode UpsideDownBinaryTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            LinkedList<(TreeNode, TreeNode)> list = new LinkedList<(TreeNode, TreeNode)>();
+            var node = root;
+            TreeNode nodeSecond = null;
+            while(node != null)
+            {
+                list.AddFirst((CloneNode(node), CloneNodeRecursive(nodeSecond)));
+                nodeSecond = node.right;
+                node = node.left;
+            }
+
+            var result = list.First.Value.Item1;
+            result.left = list.First.Value.Item2;
+            var father = result;
+            foreach ((var cNode , var leftNode) in list.Skip(1))
+            {
+                father.right = cNode;
+                cNode.left = leftNode;
+                father = cNode;
+            }
+
+            return result;
+        }
+
+        private TreeNode CloneNode(TreeNode node)
+        {
+            if (node == null)
+                return null;
+            return new TreeNode(node.val);
+        }
+
+        private TreeNode CloneNodeRecursive(TreeNode node)
+        {
+            if (node == null)
+                return null;
+            var result = new TreeNode(node.val);
+
+            var resultLeft = CloneNodeRecursive(node.left);
+            var resultRight = CloneNodeRecursive(node.right);
+            result.left = resultLeft;
+            result.right = resultRight;
+
+            return result;
+        }
+    }
+}
---
+++ BinaryTreeUpsideDownTest/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using BinaryTreeUpsideDown;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryTreeUpsideDownTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TreeNode node = new TreeNode(1);
+            node.right = new TreeNode(3);
+            node.left = new TreeNode(2);
+            node.left.right = new TreeNode(5);
+            node.left.left = new TreeNode(4);
+
+            Solution s = new Solution();
+            var result = s.UpsideDownBinaryTree(node);
+
+            Assert.AreEqual(result.val, 4);
+            Assert.AreEqual(result.left.val, 5);
+            Assert.AreEqual(result.right.val, 2);
+            Assert.AreEqual(result.right.left.val, 3);
+            Assert.AreEqual(result.right.right.val, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            var result = s.UpsideDownBinaryTree(null);
+            Assert.AreEqual(result, null);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            TreeNode node = new TreeNode(1);
+            Solution s = new Solution();
+            var result = s.UpsideDownBinaryTree(node);
+            Assert.AreEqual(result.val, 1);
+            Assert.AreEqual(result.left, null);
+            Assert.AreEqual(result.right, null);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            TreeNode node = new TreeNode(1);
+            node.right = new TreeNode(3);
+            node.left = new TreeNode(2);
+            node.left.right = new TreeNode(5);
+            node.left.left = new TreeNode(4);
+            node.right.right = new TreeNode(7);
+            node.right.left = new TreeNode(6);
+
+            Solution s = new Solution();
+            var result = s.UpsideDownBinaryTree(node);
+        
+        }
+    }
+}
