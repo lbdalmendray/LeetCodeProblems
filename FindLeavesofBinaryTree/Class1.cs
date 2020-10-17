d313f6305a3935fe7f27d79af6e472d5418c6240
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-10-16 23:41:07 -0400

    Updated

---
+++ FindLeavesofBinaryTree/Class1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindLeavesofBinaryTree
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+    public class Solution
+    {
+        public IList<IList<int>> FindLeaves(TreeNode root)
+        {
+            if (root == null)
+                return new List<IList<int>>();
+
+            var leaves = BFS(root);
+
+            LinkedList<IList<int>> result = new LinkedList<IList<int>>();
+            while(leaves .Count != 0)
+            {
+                result.AddLast(leaves.Select(e => e.val).ToList() as IList<int>);
+                LinkedList<TreeNodeFather> newLeaves = new LinkedList<TreeNodeFather>();
+                foreach (var item in leaves)
+                {
+                    if (item.Father != null)
+                    {
+                        if (item.Father.left == item)
+                            item.Father.left = null;
+                        else
+                            item.Father.right = null;
+
+                        if (item.Father.left == null && item.Father.right == null)
+                        {
+                            newLeaves.AddLast(item.Father);
+                        }
+                    }
+                }
+
+                leaves = newLeaves;
+            }
+
+            return result.ToList();
+        }
+
+        private LinkedList<TreeNodeFather> BFS(TreeNode root)
+        {
+            LinkedList<(TreeNodeFather,TreeNode)> queue = new LinkedList<(TreeNodeFather,TreeNode)>();
+            TreeNodeFather rootFather = new TreeNodeFather(root, null, false);
+            LinkedList<TreeNodeFather> result = new LinkedList<TreeNodeFather>();
+            queue.AddLast((rootFather,root));
+
+            while (queue.Count != 0)
+            {
+                (var nodeFather, var node) = queue.First.Value;
+                queue.RemoveFirst();
+
+                bool isLeave = true;
+                if (node.left != null)
+                {
+                    nodeFather.left = new TreeNodeFather(node.left, nodeFather, true);
+                    queue.AddLast((nodeFather.left as TreeNodeFather, node.left));
+                    isLeave = false;
+                }
+
+                if (node.right != null)
+                {
+                    nodeFather.right = new TreeNodeFather(node.right, nodeFather, false);
+                    queue.AddLast((nodeFather.right as TreeNodeFather, node.right));
+                    isLeave = false;
+                }
+
+                if (isLeave)
+                {
+                    result.AddLast(nodeFather);
+                }
+            }
+            return result;
+        }
+    }
+
+    internal class TreeNodeFather : TreeNode
+    {
+        public TreeNodeFather Father { get; internal set; }
+
+        public TreeNodeFather(TreeNode node , TreeNodeFather father , bool isLeftChild)
+            : base(node.val)
+        {
+            Father = father;
+            if (father != null)
+            {
+                if (isLeftChild)
+                    father.left = node;
+                else
+                    father.right = node;
+            }
+        }
+    }
+}
---
+++ FindLeavesofBinaryTreeTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FindLeavesofBinaryTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindLeavesofBinaryTreeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /*
+                         1            1         1
+                        / \          /
+                       2   3   ---> 2     ---> 
+                      / \            
+                     4   5           
+            */
+
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+
+            Solution s = new Solution();
+            IList<IList<int>> result = s.FindLeaves(root);
+            List<int> list = new List<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(3);
+            CollectionAssert.AreEquivalent(result[0].ToArray(), list.ToArray());
+
+            list = new List<int>();
+            list.Add(2);
+            CollectionAssert.AreEquivalent(result[1].ToArray(), list.ToArray());
+
+            list = new List<int>();
+            list.Add(1);
+            CollectionAssert.AreEquivalent(result[2].ToArray(), list.ToArray());
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            /*
+                        EMPTY        
+            */
+
+            Solution s = new Solution();
+            IList<IList<int>> result = s.FindLeaves(null);
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        //[]
+    }
+}
