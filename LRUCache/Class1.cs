f627caa92f2a072c769195698cbab394872292ac
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-10-16 23:58:25 -0400

    Updated

---
+++ LRUCache/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache
+{
+    public class LRUCache
+    {
+        private readonly int capacity;
+        private readonly LinkedList<int> keyOrder = new LinkedList<int>();
+        private readonly Dictionary<int, Info> dictionary;
+
+        public LRUCache(int capacity)
+        {
+            if (capacity < 0)
+                capacity = 0;
+
+            this.capacity = capacity;
+            dictionary = new Dictionary<int, Info>(capacity);
+        }
+
+        public int Get(int key)
+        {
+            if (dictionary.TryGetValue(key, out var info))
+            {
+                PutFirst(key, info);
+                
+                return info.Value;
+            }
+            else
+                return -1;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (dictionary.TryGetValue(key, out var info))
+            {
+                PutFirst(key, info);
+                info.Value = value;
+            }
+            else
+            {
+                if (capacity == 0)
+                    return;
+
+                if (keyOrder.Count == capacity)
+                    RemoveLast();
+                var node = keyOrder.AddFirst(key);
+                info = new Info { Value = value, Node = node };
+                dictionary.Add(key, info);                
+            }
+        }        
+
+        private void PutFirst(int key, Info info)
+        {
+            LinkedListNode<int> node = info.Node;
+            keyOrder.Remove(node);
+            info.Node = keyOrder.AddFirst(key);
+        }
+
+        private void RemoveLast()
+        {
+            var key = keyOrder.Last.Value;
+            keyOrder.RemoveLast();
+            dictionary.Remove(key);
+        }
+
+        private class Info
+        {
+            public int Value { get; internal set; }
+            public LinkedListNode<int> Node { get; internal set; }
+        }
+    }
+
+    
+}
---
+++ LRUCacheTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LRUCache;
+
+namespace LRUCacheTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            LRUCache.LRUCache cache = new LRUCache.LRUCache(2);
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            Assert.AreEqual(cache.Get(1), 1);           // returns 1
+            cache.Put(3, 3);                            // evicts key 2
+            Assert.AreEqual(cache.Get(2), -1);          // returns -1 (not found)
+            cache.Put(4, 4);                            // evicts key 1
+            Assert.AreEqual(cache.Get(1), -1);          // returns -1 (not found)
+            Assert.AreEqual(cache.Get(3), 3);           // returns 3
+            Assert.AreEqual(cache.Get(4), 4);           // returns 4
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            LRUCache.LRUCache cache = new LRUCache.LRUCache(0);
+            /*
+             ["LFUCache","put","get"]
+[[0],[0,0],[0]]
+             */
+            cache.Put(0, 0);
+            cache.Get(0);
+        }
+    }
+}
