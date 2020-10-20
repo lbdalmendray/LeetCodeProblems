9c9ee570e048925627d15c6d6ad37a0edb8bd0de
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-10-19 21:22:39 -0400

    Updated

---
+++ LetterCombinationsofaPhoneNumber/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterCombinationsofaPhoneNumber
+{
+    public class Solution
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            LinkedList<string> result = new LinkedList<string>();
+
+            char[] current = new char[digits.Length];
+            int[] states = Enumerable.Repeat(-1, digits.Length).ToArray();
+            int index = 0;
+            while(true)
+            {
+                if ( states[index]!= 2)
+                {
+                    states[index]++;
+                }
+                else
+                {
+                    if (index == 0)
+                        break;
+                    states[index] = -1;
+                    index--;
+                    continue;
+                }
+
+                current[index] = GetChar(digits[index], states[index]);
+
+                if ( index == digits.Length-1)
+                {
+                    result.AddLast(new string(current));
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+
+            return result.ToList();
+        }
+
+        private char GetChar(char charNumber, int index)
+        {
+            int number = charNumber - '2';
+            int sum = number * 3 + index;
+            return (char)('a' + sum);
+        }
+    }
+}
---
+++ LetterCombinationsofaPhoneNumberTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using LetterCombinationsofaPhoneNumber;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LetterCombinationsofaPhoneNumberTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            var result = s.LetterCombinations("23");
+            foreach (var item in result)
+            {
+                Assert.IsTrue(new string[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }
+                .Contains(item));
+            }
+        }
+    }
+}
