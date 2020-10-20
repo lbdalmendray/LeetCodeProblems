deedceb3926614809beb2f3988ed241623ec38fd
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-10-19 21:05:18 -0400

    Updated

---
+++ IntersectionofTwoArrays/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectionofTwoArrays
+{
+    public class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+            foreach (var item in nums1)
+            {
+                if( !dict.ContainsKey(item))
+                {
+                    dict.Add(item, false);
+                }
+            }
+
+            foreach (var item in nums2)
+            {
+                if (dict.ContainsKey(item))
+                    dict[item] = true;
+            }
+
+            return dict.Where(kv => kv.Value).Select(e=>e.Key).ToArray();
+        }
+    }
+}
---
+++ IntersectionofTwoArraysTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntersectionofTwoArrays;
+
+namespace IntersectionofTwoArraysTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            var result = s.Intersection(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 });
+            Array.Sort(result);
+            int [] compareArray = new int[] { 2 };
+            Array.Sort(compareArray);
+            CollectionAssert.AreEquivalent(result, compareArray);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            var result = s.Intersection(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 });
+            Array.Sort(result);
+            int[] compareArray = new int[] { 9, 4 };
+            Array.Sort(compareArray);
+            CollectionAssert.AreEquivalent(result, compareArray);
+        }
+    }
+}
