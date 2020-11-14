502356d8ba97bc09cd4176a027f3655dcd96a2dc
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-11-14 16:30:13 -0500

    Updated

---
+++ ShortestWordDistance/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestWordDistance
+{
+    public class Solution
+    {
+        public int ShortestDistance(string[] words, string word1, string word2)
+        {
+            int i = 0;
+            int state = -1;
+            for (; i < words.Length; i++)
+            {
+                if ( words[i] == word1 )
+                {
+                    state = 1;
+                    break;
+                }
+                else if ( words[i] == word2)
+                {
+                    state = 2;
+                    break;
+                }
+            }
+            int distance = int.MaxValue;
+            for (int j = i+1; j < words.Length ; j++)
+            {
+                if ( state == 1)
+                {
+                    if (words[j] == word1)
+                    {
+                        i = j;
+                    }
+                    else if (words[j] == word2)
+                    {
+                        distance = Math.Min(distance, j - i);
+                        state = 2;
+                        i = j;
+                    }
+                }
+                else
+                {
+                    if (words[j] == word2)
+                    {
+                        i = j;
+                    }
+                    else if (words[j] == word1)
+                    {
+                        distance = Math.Min(distance, j - i);
+                        state = 1;
+                        i = j;
+                    }
+                }
+            }
+            return distance;
+        }
+    }
+}
---
+++ ShortestWordDistanceTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShortestWordDistance;
+
+namespace ShortestWordDistanceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            var result = s.ShortestDistance(new string[] { "practice", "makes", "perfect", "coding", "makes" }, "coding", "practice");
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            var result = s.ShortestDistance(new string[] { "practice", "makes", "perfect", "coding", "makes" }, "makes", "coding");
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
