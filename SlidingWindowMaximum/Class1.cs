e19fcf01ccd43495c6b1114f1c7ac147e9a3c99d
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-11-15 14:56:43 -0500

    Updated

---
+++ SlidingWindowMaximum/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingWindowMaximum
+{
+    public class Solution
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            int[] leftMax = new int[nums.Length];
+            int[] rightMax = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % k == 0)
+                    leftMax[i] = nums[i];
+                else
+                {
+                    leftMax[i] = Math.Max(leftMax[i - 1], nums[i]);
+                }    
+            }
+
+            rightMax[rightMax.Length - 1] = nums[nums.Length - 1];
+
+            for (int i = rightMax.Length - 2; i > -1; i--)
+            {
+                if ( (i+1) % k == 0 )
+                {
+                    rightMax[i] = nums[i];
+                }
+                else
+                {
+                    rightMax[i] = Math.Max(rightMax[i + 1], nums[i]);
+                }
+            }
+            int length = nums.Length - (k - 1);
+            int[] result = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int rightPart = rightMax[i];
+                int leftPart = leftMax[i + k - 1];
+                int max = Math.Max(rightPart, leftPart);
+                result[i] = max;
+            }
+
+            return result;            
+        }
+    }
+}
---
+++ SlidingWindowMaximumTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SlidingWindowMaximum;
+
+namespace SlidingWindowMaximumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            var result = s.MaxSlidingWindow(nums: new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, k: 3);
+            CollectionAssert.AreEquivalent(result, new int[] { 3, 3, 5, 5, 6, 7 });
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            var result = s.MaxSlidingWindow(nums: new int[] { 1 }, k: 1);
+            CollectionAssert.AreEquivalent(result, new int[] { 1 });
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            var result = s.MaxSlidingWindow(nums: new int[] { 1, -1 }, k: 1);
+            CollectionAssert.AreEquivalent(result, new int[] { 1, -1 });
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            var result = s.MaxSlidingWindow(nums: new int[] { 9, 11 }, k: 2);
+            CollectionAssert.AreEquivalent(result, new int[] { 11 });
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            var result = s.MaxSlidingWindow(nums: new int[] { 4, -2 }, k: 2);
+            CollectionAssert.AreEquivalent(result, new int[] { 4 });
+        }
+
+    }
+}
