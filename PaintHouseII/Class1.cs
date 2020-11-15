74d93458d3b6fb4c8fe0b3765a155e95bcb48e0e
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-11-15 15:56:11 -0500

    Updated

---
+++ PaintHouseII/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintHouseII
+{
+    public class Solution
+    {
+        public int MinCost(int[][] costs)
+        {
+            if (costs == null || costs.Length == 0)
+                return 0;
+
+            int n = costs.Length;
+            int k = costs[0].Length;
+
+            int?[,] infos = new int?[n, k];
+            var branches = Enumerable.Range(0, k);
+            for (int i = n - 1; i > 0; i--)
+            {
+                branches.Select(e => Calculate(i, e, infos, costs)).Min();
+            }
+            var result = branches.Select(e => Calculate(0, e, infos, costs)).Min();
+            return result;
+        }
+
+        private int Calculate(int index, int color, int?[,] infos, int[][] costs)
+        {
+            if (infos[index, color].HasValue)
+                return infos[index, color].Value;
+
+            var result = costs[index][color];
+            if (index < infos.GetLength(0) - 1)
+            {
+                var indexPlus1 = index + 1;
+                var branches = Enumerable.Range(0, costs[0].Length).Where(e => e != color);
+                var minValue = branches.Select(e => Calculate(indexPlus1, e, infos, costs)).Min();
+                result += minValue;
+            }
+
+            infos[index, color] = result;
+            return result;
+        }
+    }
+}
---
+++ PaintHouseIITest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaintHouseII;
+
+namespace PaintHouseIITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[][] input = new int[][]
+             {
+                new int[]{ 17, 2, 17 }
+                ,new int[]{16, 16, 5 }
+                ,new int[]{14, 3, 19 }
+             };
+
+            Solution s = new Solution();
+            var result = s.MinCost(input);
+            Assert.AreEqual(result, 10);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[][] input = new int[][]
+             {
+                new int[]{ 1, 5, 3 }
+                ,new int[]{ 2, 9, 4 }
+             };
+
+            Solution s = new Solution();
+            var result = s.MinCost(input);
+            Assert.AreEqual(result, 5);
+        }
+    }
+}
