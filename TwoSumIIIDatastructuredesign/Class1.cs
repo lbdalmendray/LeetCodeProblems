79e9a6ec70f06957ff449462a45927028c2b7040
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-11-15 17:23:37 -0500

    Updated

---
+++ TwoSumIIIDatastructuredesign/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSumIIIDatastructuredesign
+{
+    public class TwoSum
+    {
+        Dictionary<int, int[]> numbersFreq;
+
+        /** Initialize your data structure here. */
+        public TwoSum()
+        {
+            numbersFreq = new Dictionary<int, int[]>();
+        }
+
+        /** Add the number to an internal data structure.. */
+        public void Add(int number)
+        {
+            if (!numbersFreq.TryGetValue(number, out var array))
+            {
+                array = new int[] { 1 };
+                numbersFreq[number] = array;
+            }
+            else
+            {
+                array[0]++;
+            }
+        }
+
+        /** Find if there exists any pair of numbers which sum is equal to the value. */
+        public bool Find(int value)
+        {
+            foreach (var item in numbersFreq)
+            {
+                int other = value - item.Key ;
+                if (other == item.Key)
+                {
+                    if (item.Value[0] > 1)
+                        return true;
+                }
+                else if (numbersFreq.ContainsKey(other))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * Your TwoSum object will be instantiated and called as such:
+     * TwoSum obj = new TwoSum();
+     * obj.Add(number);
+     * bool param_2 = obj.Find(value);
+     */
+}
---
+++ TwoSumIIIDatastructuredesignTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSumIIIDatastructuredesign;
+
+namespace TwoSumIIIDatastructuredesignTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TwoSum s = new TwoSum();
+            s.Add(1); 
+            s.Add(3);
+            s.Add(5);
+            Assert.IsTrue(s.Find(4));
+            Assert.IsFalse(s.Find(7));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            TwoSum s = new TwoSum();
+            s.Add(3);
+            s.Add(1);
+            s.Add(2);
+            Assert.IsTrue(s.Find(3));
+            Assert.IsFalse(s.Find(6));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            TwoSum s = new TwoSum();
+            s.Add(0);
+            s.Add(-1);
+            s.Add(1);
+            Assert.IsTrue(s.Find(0));
+        }
+    }
+}
