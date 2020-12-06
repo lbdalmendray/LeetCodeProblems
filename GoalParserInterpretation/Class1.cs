248d3bce732ae667e6881923c84d0a919ded99aa
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2020-12-06 00:42:40 -0500

    Updated

---
+++ GoalParserInterpretation/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace GoalParserInterpretation
+{
+    public class Solution
+    {
+        public string Interpret(string command)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < command.Length; i++)
+            {
+                if (command[i] == 'G')
+                    result.Append('G');
+                else // if ( command[i]=='(')
+                {
+                    i++;
+                    if( command[i] == ')')
+                    {
+                        result.Append('o');
+                    }
+                    else
+                    {
+                        i+=2;
+                        result.Append("al");
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
