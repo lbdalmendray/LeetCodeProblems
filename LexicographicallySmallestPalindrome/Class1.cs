5ba9864c025a03e69de7756acb79a54c4b581262
Luis Benavides Dalmendray <lbdalmendray@yahoo.com>
2023-05-22 17:36:49 -0400

    Lexicographically Smallest Palindrome

---
+++ LexicographicallySmallestPalindrome/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace LexicographicallySmallestPalindrome
+{
+    public class Solution
+    {
+        public string MakeSmallestPalindrome(string s)
+        {
+            int index = s.Length / 2 - 1;
+            char[] sArray = s.ToArray();
+
+            for (; index > -1; index--)
+            {
+                if (sArray[index] < sArray[sArray.Length - 1 - index])
+                {
+                    sArray[sArray.Length - 1 - index] = sArray[index];
+                }
+                else
+                {
+                    sArray[index] = sArray[sArray.Length - 1 - index];
+                }
+            }
+
+            return new string(sArray);
+        }
+    }
+}
