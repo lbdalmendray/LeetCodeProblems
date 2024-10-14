3ff831fff09582d435a974deb354c77246362928
Luis Benavides Dalmendray <lbdalmendray@outlook.com>
2024-10-14 00:09:08 -0400

    MinimumAdjacentSwapstoMakeaValidArray solution added.

---
+++ MinimumAdjacentSwapstoMakeaValidArray/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace MinimumAdjacentSwapstoMakeaValidArray;
+
+public class Solution
+{
+    public int MinimumSwaps(int[] nums)
+    {
+        int minIndex = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < nums[minIndex])
+            {
+                minIndex = i;
+            }
+        }
+
+        int maxIndex = nums.Length - 1;
+        for (int i = nums.Length - 2; i > -1; i--)
+        {
+            if (nums[i] > nums[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+
+        if (minIndex == maxIndex)
+            return 0;
+        if ( minIndex < maxIndex)
+            return (nums.Length - 1 - maxIndex) + ( minIndex); 
+        else
+            return (nums.Length - 1 - maxIndex) + (minIndex) - 1;
+    }
+}
