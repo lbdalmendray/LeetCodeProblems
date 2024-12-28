24ad038e5c2971dc9f625f8b6960e5cfd006d1d9
Luis Benavides Dalmendray <lbdalmendray@outlook.com>
2024-12-27 20:35:57 -0500

    Zigzag Conversion: Faster solution and also better memory usage solution added.

--- ZigzagConversion/Class1.cs
+++ ZigzagConversion/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace ZigzagConversion;
@@ -12,19 +13,83 @@ s consists of English letters (lower-case and upper-case), ',' and '.'.
 
 public class Solution
 {
-    public string Convert(string s, int numRows)
+    public unsafe string Convert(string s, int numRows)
     {
         if (numRows > s.Length)
-            return s; 
-        else if (numRows == 1)
             return s;
+        else if (numRows != 1)
+        {
+            char* result = (char*)Marshal.AllocHGlobal((s.Length + 1) * sizeof(char));
+            result[s.Length] = '\0';
+
+            var numRowsLess1 = numRows - 1;
+            int m = 0;
+            int j;
+            var delta = 2 * (numRowsLess1);
+            
+            //// CASE INDEX == 0
+            j = 0;
+            while (j < s.Length)
+            {
+                result[m++] = s[j];
+                j += delta;
+            }
+
+            //// CASE : INTERNAL INDEXES 
+
+            for (int i = 1; i < numRowsLess1; i++)
+            {
+                bool IsGoingToSecondPart = true;
+                j = i;
+                int secondPartDelta = 2 * (numRowsLess1 - i);
+                int firstPartDelta = 2 * i;
+                while (j < s.Length)
+                {
+                    result[m++] = s[j];
+
+                    if (IsGoingToSecondPart)
+                    {
+                        j += secondPartDelta;
+                        IsGoingToSecondPart = false;
+                    }
+                    else
+                    {
+                        j += firstPartDelta;
+                        IsGoingToSecondPart = true;
+                    }
+                }
+            }
+
+            //// CASE INDEX == numRows - 1
+
+            j = numRowsLess1;
+            while (j < s.Length)
+            {
+                result[m++] = s[j];
+                j += delta;
+            }
+
+            return new string(result);
+        }
         else
-            return new string(ConvertEnumerable(s, numRows));
+            return s;
     }
 
-    private Char[] ConvertEnumerable(string s, int numRows)
+    public unsafe string Convert2(string s, int numRows)
     {
-        Char[] result = new char[s.Length];
+        if (numRows > s.Length)
+            return s;
+        else if (numRows != 1)
+            return new string(ConvertEnumerable(s, numRows));
+        else
+            return s;
+    }
+
+    private unsafe char* ConvertEnumerable(string s, int numRows)
+    {
+        char* result = (char*)Marshal.AllocHGlobal((s.Length + 1) * sizeof(char));
+        result[s.Length] = '\0';
+
         int m = 0;
         for (int i = 0; i < numRows; i++)
         {
