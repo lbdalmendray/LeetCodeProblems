da2496456b6f575943f9849bde1dd5a61a986a23
Luis Benavides Dalmendray <lbdalmendray@outlook.com>
2024-12-30 17:20:12 -0500

    ReverseInteger better solution.

--- ReverseInteger/Class1.cs
+++ ReverseInteger/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace ReverseInteger;
 
@@ -6,44 +7,48 @@ public class Solution
 {
     const string int32MinValueString = "-2147483648";
     const string int32MaxValueString = "2147483647";
-    public int Reverse(int x)
+    public unsafe int Reverse(int x)
     {
         string xString = x.ToString();
-        StringBuilder xStringBuilder = new StringBuilder(xString.Length);
+        //char[] xResultString = new char[xString.Length];
+
+        char* xResultString = (char*)Marshal.AllocHGlobal((xString.Length + 1) * sizeof(char));
+        xResultString[xString.Length] = '\0';
+
         if (x < 0)
         {
-            xStringBuilder.Append('-');
+            xResultString[0] = '-';
             for (int i = xString.Length - 1; i > 0; i--)
-                xStringBuilder.Append(xString[i]);
+                xResultString[xString.Length - i] = xString[i];
         }
         else
         {
             for (int i = xString.Length - 1; i > -1; i--)
-                xStringBuilder.Append(xString[i]);
+                xResultString[xString.Length - 1 - i] = xString[i];
         }
 
         string int32MinMaxValueString = x >= 0 
             ? int32MaxValueString
             : int32MinValueString;
 
-        if (int32MinMaxValueString.Length < xStringBuilder.Length)
+        if (int32MinMaxValueString.Length < xString.Length)
         {
             return 0;
         }
-        else if (int32MinMaxValueString.Length > xStringBuilder.Length)
-            return int.Parse(xStringBuilder.ToString());
+        else if (int32MinMaxValueString.Length > xString.Length)
+            return int.Parse(new string(xResultString));
         else
         {
-            for (int i = 0 ; i < xStringBuilder.Length; i++)
+            for (int i = 0 ; i < xString.Length; i++)
             {
-                if (int32MinMaxValueString[i] == xStringBuilder[i])
+                if (int32MinMaxValueString[i] == xResultString[i])
                     continue;
-                else if (int32MinMaxValueString[i] < xStringBuilder[i])
+                else if (int32MinMaxValueString[i] < xResultString[i])
                     return 0;
                 else
                     break;
             }
-            return int.Parse(xStringBuilder.ToString());
+            return int.Parse(new string(xResultString));
         }
     }
 }
