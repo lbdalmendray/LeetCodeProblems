66f3eddfcd81afed82045599745f81e7fd9e34c1
Luis Benavides Dalmendray <lbdalmendray@outlook.com>
2025-03-02 00:39:21 -0500

    TopKFrequentElements' solution added.

--- TopKFrequentElements/Class1.cs
+++ TopKFrequentElements/Class1.cs
@@ -6,25 +6,75 @@ public class Solution
 {
     public int[] TopKFrequent(int[] nums, int k)
     {
-        /// CONSTANT OPERATIONS 
-        int[] positiveIntCounterArray = new int[10_000];
-        int[] negativeIntCounterArray = new int[10_000];
-        ////////////////////////////////////////////////
+        Dictionary<int, int[]> counterDict = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int num = nums[i];
+            if ( counterDict.TryGetValue(num, out var counterArray))
+            {
+                counterArray[0]++;
+            }
+            else
+            {
+                counterArray = new int[1] { 1 };
+                counterDict.Add(num, counterArray);
+            }
+        }
+
+        List<int>[] CounterGroups = new List<int>[(nums.Length + 1)];
+
+
+        foreach (var counterKV in counterDict)
+        {
+            if (CounterGroups[counterKV.Value[0]] == null)
+                CounterGroups[counterKV.Value[0]] = new();
+            CounterGroups[counterKV.Value[0]].Add(counterKV.Key); 
+        }
+
+        List<int> result = new List<int>();
+
+        for (int i = CounterGroups.Length - 1;
+            i > -1 && result.Count < k; i--)
+        {
+            if (CounterGroups[i] != null)
+                result.AddRange(CounterGroups[i]);
+        }
+
+        return result
+            // .Take(k) // IT IS NOT NECESSARY BECAUSE THERE IS ONLY ONE SOLUTION,
+                        // SO IT IS NOT POSSIBLE TO ADD X AMOUNT OF VALUES TO result
+                        // THAT EXCEEDS k WHEM result HAD Y ELEMENTS BECAUSE THAT MEANS
+                        // THAT THERE WOULD SOME ELEMENTS IN X, LET SAY X1 ELEMENTS
+                        // ( WHERE X1 < X ) THAT CAN BE ADDED TO Y SO X1 + Y = K BUT IF WE
+                        // TAKE ANY ELEMENT OF X - X1 AND REPLACE ANY ELEMENT OF X1 THEN 
+                        // WE CAN CREATE ANOTHER SOLUTION X1REPLACED + Y, AND IT IS
+                        // NOT POSSIBLE.
+            .ToArray();
+
+    }
+
+    public int[] TopKFrequent1(int[] nums, int k)
+    {
+        Dictionary<int, int[]> dictCounter = new();
+
 
         /// O(N)
         for (int i = 0; i < nums.Length; i++)
         {
             int num = nums[i];
-            if (num >= 0)
-                positiveIntCounterArray[num]++;
+            if (dictCounter.TryGetValue(num, out var counterArray))
+            {
+                counterArray[0]++;
+            }
             else
             {
-                negativeIntCounterArray[Math.Abs(num)]++;
-            }
+                counterArray = new int[1] { 1 };
+                dictCounter[num] = counterArray;
+            }                
         }
 
         HashSet<int> assignedNumbers = new HashSet<int>(k);
-        SortedDictionary<int, HashSet<int>> sortedDict = new();
+        SortedDictionary<int, List<int>> sortedDict = new();
 
         //// O(N*LOG(K))
         for (int i = 0; i < nums.Length; i++)
@@ -33,30 +83,30 @@ public class Solution
 
             if (assignedNumbers.Count < k)
             {
-                assignedNumbers.Add(num);
-                int counter = num >= 0 ? positiveIntCounterArray[num]
-                                        : negativeIntCounterArray[Math.Abs(num)];
-
-                if ( !sortedDict.TryGetValue(counter, out var set))
+                if (assignedNumbers.Add(num))
                 {
-                    set = new HashSet<int>();
-                    sortedDict.Add(counter, set);
+                    int counter = dictCounter[num][0];
+
+                    if (!sortedDict.TryGetValue(counter, out var set))
+                    {
+                        set = new List<int>();
+                        sortedDict.Add(counter, set);
+                    }
+                    set.Add(num);
                 }
-                set.Add(num);
             }
             else if (assignedNumbers.Contains(num))
                 continue;
             else
             {
-                int counter = num >= 0 ? positiveIntCounterArray[num] 
-                                       : negativeIntCounterArray[Math.Abs(num)];
+                int counter = dictCounter[num][0];
 
                 var firstKV = sortedDict.First();
                 if ( firstKV.Key < counter)
                 {
-                    var toRemove = firstKV.Value.First();
+                    var toRemove = firstKV.Value[firstKV.Value.Count-1];
                     assignedNumbers.Remove(toRemove);
-                    firstKV.Value.Remove(toRemove);
+                    firstKV.Value.RemoveAt(firstKV.Value.Count - 1);
                     if ( firstKV.Value.Count == 0)
                     {
                         sortedDict.Remove(firstKV.Key);
@@ -64,7 +114,7 @@ public class Solution
 
                     if (!sortedDict.TryGetValue(counter, out var set))
                     {
-                        set = new HashSet<int>();
+                        set = new List<int>();
                         sortedDict.Add(counter, set);
                     }
                     set.Add(num);
