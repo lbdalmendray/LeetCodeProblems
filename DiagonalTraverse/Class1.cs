15f834e9392130c809ea2c3893a73a951df55e7b
Luis Benavides Dalmendray <lbdalmendray@outlook.com>
2026-01-01 15:39:10 -0500

    DiagonalTraverse solution added.

---
+++ DiagonalTraverse/Class1.cs
@@ -0,0 +1,76 @@
+﻿namespace DiagonalTraverse;
+
+public class Solution
+{
+    public int[] FindDiagonalOrder(int[][] mat)
+    {
+        int m = mat.Length;
+        int n = mat[0].Length;
+
+        if (m == 1)
+            return mat[0];
+        else if (n == 1)
+        {
+            int[] result = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                result[i] = mat[i][0];
+            }
+            return result;
+        }
+        else
+        {
+            int counter = 0;
+            int total = m * n;
+            int x = 0;
+            int y = 0;
+            bool up = true;
+            int[] result = new int[total];
+            while (counter < total)
+            {
+                result[counter] = mat[x][y];
+                counter++;
+
+                if (up)
+                {
+                    if (y == n - 1)
+                    {
+                        x++;
+                        up = false;
+                    }
+                    else if (x == 0)
+                    {
+                        y++;
+                        up = false;
+                    }
+                    else
+                    {
+                        x--;
+                        y++;
+                    }
+                }
+                else /// down
+                {
+                    if (x == m - 1)
+                    {
+                        y++;
+                        up = true;
+                    }
+
+                    else if (y == 0)
+                    {
+                        x++;
+                        up = true;
+                    }
+                    else
+                    {
+                        x++;
+                        y--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DiagonalTraverseTest/Test1.cs
@@ -0,0 +1,56 @@
+﻿using DiagonalTraverse;
+
+namespace DiagonalTraverseTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution solution = new Solution();
+            var result = solution.FindDiagonalOrder([[1], [2], [3], [4]]);
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 2, 3, 4 });
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution solution = new Solution();
+            var result = solution.FindDiagonalOrder([[1,2,3,4,5]]);
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 2, 3, 4,5 });
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution solution = new Solution();
+            var result = solution.FindDiagonalOrder([[1,2,3], [4,5,6], [7,8,9]]);
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 2, 4, 7,5,3,6,8,9 });
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution solution = new Solution();
+            var result = solution.FindDiagonalOrder([[1, 2], [3,4]]);
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 2, 3, 4});
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution solution = new Solution();
+            var result = solution.FindDiagonalOrder([[1, 2], [3, 5], [4,6]]);
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 2, 3, 4 , 5, 6});
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution solution = new Solution();
+            var result = solution.FindDiagonalOrder([[1, 2, 5, 6], [3, 4, 7, 8]]);
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+        }
+    }
+}
