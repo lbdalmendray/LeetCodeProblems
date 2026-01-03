18fc346b9b4d9097ccd3ae6a912f2b227c23975b
Luis Benavides Dalmendray <lbdalmendray@outlook.com>
2026-01-02 23:47:56 -0500

    MaximumNumberofEventsThatCanBeAttended's solution added

---
+++ MaximumNumberofEventsThatCanBeAttended/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaximumNumberofEventsThatCanBeAttended;
+
+public class Solution
+{
+    public int MaxEvents(int[][] events)
+    {
+        int result = 0;
+        Array.Sort(events, Comparer<int[]>.Create((e1, e2) =>
+        {
+            return e1[0] - e2[0];
+        }));
+        SortedDictionary<int, int[]> sortedDict = new ();
+
+        int start0 = events[0][0];
+        int i = 0;
+
+        int currentDay = events[i][0];
+        i = AddAllEqualToIndexIbyStartReturnNextIndex(i, events, sortedDict);
+        
+        while( sortedDict.Count > 0 || i < events.Length)
+        {
+            if ( sortedDict.Count == 0)
+            {
+                currentDay = events[i][0];
+                i = AddAllEqualToIndexIbyStartReturnNextIndex(i, events, sortedDict);
+            }
+
+            var kv = sortedDict.First();
+            /// REMOVE THIS ONE ////
+            kv.Value[0]--;
+            if (kv.Value[0] == 0)
+            {
+                sortedDict.Remove(kv.Key);
+            }
+            //////////////////////
+
+            if (kv.Key >= currentDay)
+            {
+                result++;
+                currentDay++;
+
+                if (i < events.Length && currentDay == events[i][0])
+                {
+                    i = AddAllEqualToIndexIbyStartReturnNextIndex(i, events, sortedDict);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int AddAllEqualToIndexIbyStartReturnNextIndex(int firstIndex, int[][] events, SortedDictionary<int, int[]> sortedDict)
+    {
+        int start = events[firstIndex][0];
+        for (; firstIndex < events.Length; firstIndex++)
+        {
+            if (events[firstIndex][0] == start)
+            {
+                if (!sortedDict.TryGetValue(events[firstIndex][1], out var counter))
+                {
+                    counter = [0];
+                    sortedDict.Add(events[firstIndex][1], counter);
+                }
+                counter[0]++;
+            }
+            else
+                break;
+        }
+        return firstIndex;
+    }
+}
---
+++ MaximumNumberofEventsThatCanBeAttendedTest/Test1.cs
@@ -0,0 +1,57 @@
+﻿using MaximumNumberofEventsThatCanBeAttended;
+
+namespace MaximumNumberofEventsThatCanBeAttendedTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution solution = new Solution();
+            var result = solution.MaxEvents([[1, 2], [2, 3], [3, 4]]);
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution solution = new Solution();
+            var result = solution.MaxEvents([[1, 2], [2, 3], [3, 4], [1, 2]]);
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution solution = new Solution();
+            var result = solution.MaxEvents([[1, 2], [1, 2], [1, 3]]);
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution solution = new Solution();
+            var result = solution.MaxEvents([[1, 2], [1, 2], [1, 3]]);
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution solution = new Solution();
+            var result = solution.MaxEvents([[1, 2], [1, 2], [3, 3], [1, 5], [1, 5]]);
+            Assert.AreEqual(result, 5);
+        }
+
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution solution = new Solution();
+            var result = solution.MaxEvents([[1,4], [1,4], [3,3] , [1,4], [ 4,4], [6, 6], [6,6], [6,7]]);
+            Assert.AreEqual(result, 6);
+        }
+    }
+}
